0b14c7c7c25ab68a75e21fbef0861c2d750c627d
lihaitao <lihaitao@cnhnkj.com>
2025-08-23 18:04:11 +0800

    完善项目架构

--- notification/internal/common/constant/err.go
+++ notification/internal/common/constant/err.go
@@ -1,9 +1,9 @@
 package constant
 
-import "github.com/qq754174349/ht-frame/common/result"
+import "github.com/qq754174349/ht-frame/common/error"
 
 var (
-	NoLog     = result.Template{Code: 600, Msg: "未登录"}
-	NoReg     = result.Template{Code: 610, Msg: "未注册"}
-	RepeatReg = result.Template{Code: 611, Msg: "重复注册"}
+	NoLog     = error.Template{Code: 600, Msg: "未登录"}
+	NoReg     = error.Template{Code: 610, Msg: "未注册"}
+	RepeatReg = error.Template{Code: 611, Msg: "重复注册"}
 )
